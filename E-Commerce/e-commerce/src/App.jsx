430a720533ffd4b2de4bb533a2268c8ec79f590c
Nashy1 <nashnyembe0902@gmail.com>
2023-07-17 14:42:19 +0200

    com

--- E-Commerce/e-commerce/src/App.jsx
+++ E-Commerce/e-commerce/src/App.jsx
@@ -1,6 +1,6 @@
 import { Redirect } from 'react-router-dom';
 import './App.css';
-import Home from './pages/home';
+// import Home from './pages/Home';
 import ProductList from './pages/ProductList';
 import Product from './pages/Product';
 import Register from './pages/Register';
