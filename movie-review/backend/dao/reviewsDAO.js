13d2dfe10c993c67c14c68976da2c149e1144c67
Nashy1 <nashnyembe0902@gmail.com>
2023-07-22 11:54:04 +0200

    com

--- movie-review/backend/dao/reviewsDAO.js
+++ movie-review/backend/dao/reviewsDAO.js
@@ -1,2 +1,16 @@
 import mongodb from 'mongodb';
 const ObjectId = mongodb.ObjectId;
+
+let reviews
+
+export default class ReviewsDao {
+    static async injectDB(conn) {
+        if (reviews) {
+            return
+        }
+        try {
+            reviews = await conn.db(process.env.MOVIEREVIEWS_NS).collection('reviews')
+        } catch (e) {
+            console.error(`Unable to establish connection handle in review DAO: ${e}`)
+        }
+    }
