e55a56a7d68ccbb70b1f52a0bc8b212fbd7c888b
Nashy1 <nashnyembe0902@gmail.com>
2023-07-25 15:28:37 +0200

    com

--- e-commerce/frontend/src/pages/Login.jsx
+++ e-commerce/frontend/src/pages/Login.jsx
@@ -69,31 +69,31 @@ const Error = styled.span`
 `;
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const dispatch = useDispatch(); 
-  const {isFetching, error} = useSelector(state => state.user)
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const dispatch = useDispatch();
+    const { isFetching, error } = useSelector(state => state.user)
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    login(dispatch, { username, password });
-  }
+    const handleClick = (e) => {
+        e.preventDefault();
+        login(dispatch, { username, password });
+    }
 
-  return (
-    <Container>
-      <Wrapper>
-        <Title>SIGN IN</Title>
-        <Form>
-          <Input placeholder="username" onChange={(e) => setUsername(e.target.value)} />
-          <Input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
-          <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
-          {error && <Error>Something went wrong...</Error>}
-          <Link>FORGOT YOUR PASSWORD?</Link>
-          <Link href="register">CREATE A NEW ACCOUNT</Link>
-        </Form>
-      </Wrapper>
-    </Container>
-  );
+    return (
+        <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                    <Input placeholder="username" onChange={(e) => setUsername(e.target.value)} />
+                    <Input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+                    <Button onClick={handleClick} disabled={isFetching}>LOGIN</Button>
+                    {error && <Error>Something went wrong...</Error>}
+                    <Link>FORGOT YOUR PASSWORD?</Link>
+                    <Link href="register">CREATE A NEW ACCOUNT</Link>
+                </Form>
+            </Wrapper>
+        </Container>
+    );
 };
 
 export default Login;
