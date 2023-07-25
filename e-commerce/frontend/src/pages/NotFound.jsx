c9f7d00232342a74c15ad7e5b1e38b9d9cfa1433
Nashy1 <nashnyembe0902@gmail.com>
2023-07-25 15:28:19 +0200

    com

--- e-commerce/frontend/src/pages/NotFound.jsx
+++ e-commerce/frontend/src/pages/NotFound.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './NotFound.css';
 
 const NotFoundPage = () => {
-  return (
-    <div id="notfound">
-      <div className="notfound">
-        <div className="notfound-404">
-          <h1>404</h1>
-          <h2>Page not found</h2>
+    return (
+        <div id="notfound">
+            <div className="notfound">
+                <div className="notfound-404">
+                    <h1>404</h1>
+                    <h2>Page not found</h2>
+                </div>
+                <a href="/">Take me back!</a>
+            </div>
         </div>
-        <a href="/">Take me back!</a>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default NotFoundPage;
