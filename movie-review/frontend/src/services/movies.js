cf65e7513d19fafa3d974373fb19186a1ca42efe
Nashy1 <nashnyembe0902@gmail.com>
2023-07-28 13:18:26 +0200

    com

--- movie-review/frontend/src/services/movies.js
+++ movie-review/frontend/src/services/movies.js
@@ -3,34 +3,34 @@ import axios from "axios"
 class MovieDataService {
 
     getAll(page = 0) {
-        return axios.get(`https://moviereviews1.onrender.com//movies?page=${page}`)        
+        return axios.get(`https://moviereviews1.onrender.com/movies?page=${page}`)        
     }
 
     get(id) {
-        return axios.get(`https://moviereviews1.onrender.com//movies/id/${id}`)        
+        return axios.get(`https://moviereviews1.onrender.com/movies/id/${id}`)        
     }
 
     find(query, by = "title", page = 0) {
-        return axios.get (`https://moviereviews1.onrender.com//movies?${by}=${query}&page=${page}`)
+        return axios.get (`https://moviereviews1.onrender.com/movies?${by}=${query}&page=${page}`)
     }
 
     createReview(data) {
-        return axios.post("https://moviereviews1.onrender.com//movies/review", data)
+        return axios.post("https://moviereviews1.onrender.com/movies/review", data)
     }
 
     updateReview(data) {
-        return axios.put("https://moviereviews1.onrender.com//movies/review", data)
+        return axios.put("https://moviereviews1.onrender.com/movies/review", data)
     }
 
     deleteReview(id, userId) {
         return axios.delete (
-            "https://moviereviews1.onrender.com//movies/review",
+            "https://moviereviews1.onrender.com/movies/review",
             {data:{review_id: id, user_id: userId}}
         )
     }
 
     getRatings() {
-        return axios.get("http://https://moviereviews1.onrender.com//movies/ratings")
+        return axios.get("https://moviereviews1.onrender.com/movies/ratings")
     }
 }
 
