207024677e5d0ccbf64bba9a177c6fc6e5f20fe1
Nashy1 <nashnyembe0902@gmail.com>
2023-11-09 11:58:35 +0200

    com

--- Socia/Back-end/server/index.js
+++ Socia/Back-end/server/index.js
@@ -8,3 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
+/*Configuration */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
