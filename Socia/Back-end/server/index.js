332c26454410f7b1b3286b2a9dc5170fc4ef63e9
Nashy1 <nashnyembe0902@gmail.com>
2023-11-09 12:04:07 +0200

    com

--- Socia/Back-end/server/index.js
+++ Socia/Back-end/server/index.js
@@ -19,4 +19,6 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(morgan);
 app.use(bodyParser.json({limit:"30mb"}));
-app.use(bodyParser,urlencoded({limit:"30mb", extended}))
+app.use(bodyParser,urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+app.use("/assets",express.static(path.join(__dirname,'public/assets')))
