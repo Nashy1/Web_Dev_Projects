f9b93b8222a621eeaccbf25ad6287916aa411fbe
Nashy1 <nashnyembe0902@gmail.com>
2023-11-09 11:59:18 +0200

    com

--- Socia/Back-end/server/index.js
+++ Socia/Back-end/server/index.js
@@ -11,4 +11,6 @@ import { fileURLToPath } from "url";
 
 /*Configuration */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
