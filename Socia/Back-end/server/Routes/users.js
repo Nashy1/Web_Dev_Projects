d98f5ac9188d6edd7dd03bb4f998a8bda9bad234
Nashy1 <nashnyembe0902@gmail.com>
2023-11-13 14:58:10 +0200

    com

--- Socia/Back-end/server/Routes/users.js
+++ Socia/Back-end/server/Routes/users.js
@@ -2,7 +2,7 @@ import { express } from "express";
 import{
     getUser,
     getUserFriends,
-    assRemoveFriend,
+    addRemoveFriend,
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -11,3 +11,7 @@ const router = express.Router();
 
 /*READ */
 router.get("/:id", verifyToken,getUser);
+router.get("/:id/friends",verifyToken,getUserFriends);
+
+/*UPDATE */
+router.patch("/:id/:friendId", verifyToken,addRemoveFriend)
