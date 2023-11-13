384986f7653d4a6fffae7b8b901f49673e491eb9
Nashy1 <nashnyembe0902@gmail.com>
2023-11-13 15:09:36 +0200

    com

--- Socia/Back-end/server/controllers/users.js
+++ Socia/Back-end/server/controllers/users.js
@@ -32,3 +32,34 @@ export const getUserFriends = async(req,res)=>{
         res.status(404).json({msg: err.msg});
     }
 }
+
+
+/*UPDATE */
+export const addRemoveFriend = async(req,res)=>{
+    try{
+        const {id,friendId}= req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if (user.friends.includes(friendId)){
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends =friend.friends.filter((id) => id !== friendId);
+        }else{
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend,save();
+
+        const formattedFriends = friends.map(
+            ({_id,firstName,lastName,occupation,location,picturePath})=>{
+            return {_id,firstName,lastName,occupation,location,picturePath}
+        });
+
+        res.status(200).json(formattedFriends);
+
+    }catch(err){
+        res.status(404).json({msg: err.msg});
+    }
+}
