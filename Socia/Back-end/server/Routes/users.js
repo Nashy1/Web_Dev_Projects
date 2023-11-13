08b94a2a14e1c94a599f3ee892144bec1569090b
Nashy1 <nashnyembe0902@gmail.com>
2023-11-13 14:46:09 +0200

    com

--- Socia/Back-end/server/Routes/users.js
+++ Socia/Back-end/server/Routes/users.js
@@ -10,4 +10,4 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 /*READ */
-router.get("/id", verifyToken,getUser);
+router.get("/:id", verifyToken,getUser);
