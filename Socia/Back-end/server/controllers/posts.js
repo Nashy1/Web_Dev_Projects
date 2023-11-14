00e660bac883900baa2c0d6b84fdc728e172a3c4
Nashy1 <nashnyembe0902@gmail.com>
2023-11-14 09:42:37 +0200

    com

--- Socia/Back-end/server/controllers/posts.js
+++ Socia/Back-end/server/controllers/posts.js
@@ -40,3 +40,43 @@ export const getFeedPosts = async(req,res)=>{
         res.status(404).json({msg:err.msg})
     }
 }
+
+export const getUserPosts = async(req,res)=>{
+    try{
+        const{userId} = req.params;
+        const post =await Post.find({userId});//this is find all the posts by userId.
+
+        res.status(200).json(post);
+
+    }catch(err){
+        res.status(404).json({msg:err.msg})
+    }
+}
+
+
+/*UPDATE*/
+ export const likePost =async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const{userId} = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if(isLiked){
+            post.likes.delete(userId);
+        }else{
+            post.likes.set(userId, true);
+    }
+
+    const updatedPost =await Post.findByIdUpdate(
+        id,
+        {likes: post.likes},
+        {new:true}
+    );
+
+        res.status(200).json(updatedPosts);
+
+    }catch(err){
+        res.status(404).json({msg:err.msg})
+    }
+}
