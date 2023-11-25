90fa3c9c5e9d4bb0e17d5810cc3b9e37fdcf0ba8
Nashy1 <nashnyembe0902@gmail.com>
2023-11-25 15:32:40 +0200

    com

--- Socia/Front-end/client/src/scenes/navbar/index.jsx
+++ Socia/Front-end/client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Socio-X
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
