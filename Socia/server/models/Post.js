28287f727eaf04648b2c13ed837ed916942bb666
Nashy1 <nashnyembe0902@gmail.com>
2024-01-17 11:10:51 +0200

    com

---
+++ Socia/server/models/Post.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema(
+  {
+    userId: { //string reference to user's id from user model
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {//will refer to the userID, to specify who liked a post....
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
