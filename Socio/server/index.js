3aaa7dea01127e6443c22962a68819dd72bce169
Nashy1 <nashnyembe0902@gmail.com>
2024-04-08 09:40:23 +0200

    com

--- Socio/server/index.js
+++ Socio/server/index.js
@@ -16,7 +16,7 @@ import { createPost } from "./Controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-import { users, posts } from "./data/index.js";
+import { users} from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
